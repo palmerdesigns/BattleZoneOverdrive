a1c31125f67283fc0e0a0274145a6583492c0b02
Brandon Palmer <brandonpalmerdesigns@gmail.com>
2023-05-13 22:02:41 -0700

    Added FlatKit Shaders

--- Assets/Scripts/Interactibles/Keypad.cs
+++ Assets/Scripts/Interactibles/Keypad.cs
@@ -8,7 +8,7 @@ public class Keypad : Interactible
     //This function is where we will design our inteaction using code
     protected override void Interact()
     {
-        Debug.Log("Interacted with" + gameObject.name);
+        //Debug.Log("Interacted with" + gameObject.name);
     }
 
 
